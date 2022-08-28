6cdb0694c8df10feccfbd69b387c002e99a18e29
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-08-28 19:58:22 -0400

    Functionality added

--- src/App.js
+++ src/App.js
@@ -22,8 +22,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+        {/* <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> */}
+        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
+        {/* <Backdrop show={this.state.modalIsOpen} /> */}
+        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
