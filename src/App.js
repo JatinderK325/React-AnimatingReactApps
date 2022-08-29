bfc8d17a6a77d33bf1d5c9eb948965273a6c7c7b
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-08-29 17:03:21 -0400

    Functionality added for ReactTransitionGroup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Transition from "react-transition-group/Transition";
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -7,7 +7,8 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -22,10 +23,39 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button className="Button" onClick={() =>
+          this.setState(prevState => ({ showBlock: !prevState.showBlock }))}>
+          Toggle
+        </button>
+        <br />
+        <Transition in={this.state.showBlock} timeout={1000} mountOnEnter unmountOnExit>
+          {/* state => entering, entered, exiting, exited */}
+          {state => (
+            <div
+              style={{
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition: 'all 1s ease-out',
+                opacity: state === 'exiting' ? 0 : 1,
+                backgroundColor: state === 'entering' ? 'green' : 'red'
+              }} />
+          )}
+        </Transition>
         {/* <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> */}
-        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
+        {/* Or {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null} */}
+        <Transition
+          in={this.state.modalIsOpen}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit >
+          {state => (
+            <Modal show={state} closed={this.closeModal} />
+          )}
+        </Transition>
+
         {/* <Backdrop show={this.state.modalIsOpen} /> */}
-        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
